7299a388c85a320e4b7da7d8a044e4b9b379bff4
GNVageesh <vageeshgn@gmail.com>
2021-05-07 16:27:10 +0530

    inital commit

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, jsonify, render_template, request
+from data import quotes
+
+app = Flask(__name__)
+
+with open('data.json') as file:
+    content = file.read()
+
+# Home Page
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('home.html')
+
+# All Quotes Pages
+
+
+@app.route('/api/v1/resource/all', methods=['GET'])
+def api_all():
+    return jsonify(content)
+
+# Api with ID
+
+
+@app.route('/api/v1/resource/quotes', methods=['GET'])
+def api_id():
+    if 'id' in request.args:
+        id = int(request.args['id'])
+    else:
+        # make a 404 page not found page
+        return "[ERROR] No ID field provided.... Please Specify an ID"
+
+    results = []
+
+    for quote in quotes:
+        if quote['id'] == id:
+            results.append(quote)
+
+    return jsonify(results)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
