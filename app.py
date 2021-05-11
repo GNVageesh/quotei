b6c243eff7620784bac93a1e1ef420b2eb51d624
GNVageesh <vageeshgn2005@gmail.com>
2021-05-11 20:36:33 +0530

    slight change

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request, url_for
 from data import quotes
 
 app = Flask(__name__)
@@ -42,6 +42,19 @@ def api_id():
             results.append(quote)
 
     return jsonify(results)
+
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
 
 
 if __name__ == "__main__":
