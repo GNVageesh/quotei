21a8a659e34ee10997459e4bb08e5171b8062615
GNVageesh <vageeshgn2005@gmail.com>
2021-05-15 08:19:33 +0530

    Created new file for custom functions

--- app.py
+++ app.py
@@ -1,20 +1,13 @@
 from flask import Flask, render_template, request, make_response
 from json import *
 import json
+from customFunctions import jsonify
 
 app = Flask(__name__)
 
 with open('data.json') as file:
     data = json.load(file)
 
-
-def jsonify(status=200, indent=2, sort_keys=True, **kwargs):
-    response = make_response(
-        dumps(dict(**kwargs), indent=indent, sort_keys=sort_keys))
-    response.headers['Content-Type'] = 'application/json; charset=utf-8'
-    response.headers['mimetype'] = 'application/json'
-    response.status_code = status
-    return response
 
 # Home Page
 
